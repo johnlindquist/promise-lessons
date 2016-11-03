22503ce418dc95041e7b1e62fcf3120274dd77c2
John Lindquist <johnlindquist@gmail.com>
2016-11-03 16:33:43 -0600

    adding systemjs

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
-const log = value => {
-    console.log(value)
-    return value
-}
-
-const write = value => {
-    document.body.innerHTML += `<div>${value}</div>`
-    return value
-}
-
-
-Promise.resolve('Hello, world!')
-    .then(log)
-    .then(write)
+(async function(){
+    const result = await Promise.resolve(10)
+    document.body.innerHTML += `<h1>You waited for ${result}!</h1>`
+})()
