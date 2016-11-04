98af23ce14f21fa5dd375546bde1a8f1afdb25bc
John Lindquist <johnlindquist@gmail.com>
2016-11-04 11:07:29 -0600

    async-chain

--- app.js
+++ app.js
@@ -10,9 +10,20 @@ const write = value => {
 
 const john = value => `${value}, John`
 
+const wait = value => {
+    return new Promise(resolve => {
+        setTimeout(()=>{
+            resolve(value)
+        },1000)
+    })
+}
+
 Promise.resolve('Hello')
     .then(log)
+    .then(wait)
     .then(write)
+    .then(wait)
     .then(john)
     .then(log)
+    .then(wait)
     .then(write)
